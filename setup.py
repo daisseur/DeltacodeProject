de0ab9337c102b48d51ab4403c1671ddda16be19
daisseur <100715068+daisseur@users.noreply.github.com>
2022-10-02 13:02:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='DeltacodeProject',
-    version='0.7.23',
+    version='0.7.231',
     author='daisseur',
     author_email='daisseur@gmail.com',
     packages=find_packages(),
