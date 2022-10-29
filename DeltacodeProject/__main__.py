fd0995d617f933a17294fe54d82222457828e149
daisseur <100715068+daisseur@users.noreply.github.com>
2022-10-29 18:18:12 +0200

    Update __main__.py

--- DeltacodeProject/__main__.py
+++ DeltacodeProject/__main__.py
@@ -1,12 +1,16 @@
 # __main__.py
 
 import sys
-from DeltacodeProject import main
+from DeltacodeProject.DeltacodeProject import main
+from DeltacodeProject import main as ui
 
 def main():
 	"""Encode & Decode"""
-	if len(sys.argv) > 1:
+	
+	if len(sys.argv) > 1 and ".ui" not in sys.argv:
 		print("A venir...")
+	elif ".ui" in sys.argv:
+		ui().run()
 	else:
 		main().run()
 if __name__ == "__main__":
