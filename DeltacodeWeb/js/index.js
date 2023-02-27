69421a0f1158f575e8aaf543448c09da4a74aa3c
daisseur <daisseur@gmail.com>
2023-02-28 00:47:44 +0100

    web update

--- DeltacodeWeb/js/index.js
+++ DeltacodeWeb/js/index.js
@@ -1,5 +1,9 @@
 const btn = document.getElementById("done");
-const result = document.getElementById("result");
+let result = document.getElementById("result");
+const string = document.getElementById("text");
+const shift = document.getElementById("shift");
+const password = document.getElementById("password");
+const encoding = document.getElementById("encoding");
 
 console.log("started");
 
@@ -9,24 +13,44 @@ function goIt(where) {
   window.location.assign(where);
 }
 
-function get_encode(password, string) {
-  var myInit = {method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({"string": string, "password": password})};
-  fetch("http://127.0.0.1:5000/DayEncoding/encode", myInit).then((res) => { 
-    return res.json() 
-  })
-  .then((jsonResponse) => {
-    console.log(jsonResponse);
-  })
-  .catch((err) => {
-    // handle error
-    console.error(err);
-  });
+async function coding(password, string, shift, encoding) {
+  var myInit = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ string: string, password: password, shift: shift }),
+  };
+  var obj = await fetch(`http://192.168.1.7:5000/${encoding}/encode`, myInit)
+    .then((res) => {
+      return res.json();
+    })
+    .then((jsonResponse) => {
+      console.log(jsonResponse);
+      return jsonResponse;
+    })
+    .catch((err) => {
+      // handle error
+      console.error(err);
+      return err;
+    });
+  console.log("222", obj)
+  return await obj
 }
 
 btn.addEventListener("click", () => {
   console.log("click");
-  get_encode("mdp", "coucou")
-  result.innerText = "DONE";
+  console.log(
+    password.value,
+    string.value,
+    parseInt(shift.value),
+    "after",
+    encoding.value
+  );
+  let data = coding(
+    password.value,
+    string.value,
+    parseInt(shift.value),
+    encoding.value
+  );
+  console.log(data);
+  result.innerText = data;
 });
